6cb3f603093bbb336f2e6e5d24a2f0458b8c9fde
斌斌斌 <1587443838@qq.com>
2019-08-01 10:33:40 +0800

    使用slot分发内容

---
+++ src/components/slot.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    这是slot组件
+
+    <slot name="one"> <p> 第1</p></slot>
+    <slot name="two"><p> 第11</p></slot>
+    <slot name="three"><p> 第111</p></slot>
+
+    <!--<slot>无名的slot</slot>-->
+    {{msg}}
+    <p>最后一个提醒</p>
+
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-slot',
+    data () {
+      return{
+        msg:"子组件的数据"
+      }
+    },
+    methods:{
+
+    }
+
+  }
+</script>
--- src/views/Demo.vue
+++ src/views/Demo.vue
@@ -3,15 +3,37 @@
      <p>父组件使用了count</p>
      {{count}}
      <my-prop :count="count" @increament="handleChange"></my-prop>
+
+     <my-slot>
+       <div slot="one">替换第一个</div>
+
+       <div slot="three">替换第3个</div>
+
+       <template slot="two">
+         <div>替换第2个</div>
+         <div>替换第2个ge</div>
+       </template>
+
+       <!--<div>替换无名的slot</div>-->
+
+     </my-slot>
+
+     <my-slot>
+       <!--渲染的数据是父组件的数据-->
+       {{msg}}
+     </my-slot>
    </div>
 </template>
 
 <script type="text/javascript">
-  import myProp from '../components/my-prop'
+  import myProp from '../components/my-prop';
+  import mySlot from '../components/slot'
+
   export default {
     data(){
       return{
-        count:20
+        count:20,
+        msg:"父组件的数据"
       }
     },
     methods:{
@@ -20,7 +42,8 @@
       }
     },
     components: {
-      'my-prop': myProp
+      'my-prop': myProp,
+      "my-slot":mySlot
     }
   }
 </script>
